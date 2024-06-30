39484c2222175b84839a9f5b9f1ebbe398a66bc3
Ahmed Maher <ahmedelseaidy22@gmail.com>
2024-06-30 15:12:44 +0300

    updating spinner component attribute

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div class="spinner">
-      <div class="dot dot1"></div>
-      <div class="dot dot2"></div>
-      <div class="dot dot3"></div>
-      <div class="dot dot4"></div>
+    <div className="spinner">
+      <div className="dot dot1"></div>
+      <div className="dot dot2"></div>
+      <div className="dot dot3"></div>
+      <div className="dot dot4"></div>
     </div>
   );
 };
